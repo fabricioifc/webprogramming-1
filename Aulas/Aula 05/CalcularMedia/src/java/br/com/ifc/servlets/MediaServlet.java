9abe47fcfc74e93cbf6a15acdf9b8ac38396ab67
Fabricio Bizotto <fabricio.bizotto@gmail.com>
2017-04-05 15:17:33 -0300

    commit

--- Aulas/Aula 05/CalcularMedia/src/java/br/com/ifc/servlets/MediaServlet.java
+++ Aulas/Aula 05/CalcularMedia/src/java/br/com/ifc/servlets/MediaServlet.java
@@ -2,6 +2,7 @@ package br.com.ifc.servlets;
 
 import br.com.ifc.util.Media;
 import java.io.IOException;
+import java.text.DecimalFormat;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -21,18 +22,13 @@ public class MediaServlet extends HttpServlet {
         double n3 = Double.parseDouble(request.getParameter("n3"));
         double n4 = Double.parseDouble(request.getParameter("n4"));
 
-        String media = new Media(new double[]{n1, n2, n3, n4}).calcularMedia();
+        double[] notas = new double[]{n1, n2, n3, n4};
+        
+        Media minhaClasseDeMedia = new Media(notas);
 
-        request.setAttribute("media", media);
+        request.setAttribute("media", minhaClasseDeMedia.calcularMedia());
         request.getRequestDispatcher("resultado.jsp").forward(request, response);
 
-    }
-
-    @Override
-    protected void doGet(HttpServletRequest request,
-            HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
     }
 
     @Override
--- Aulas/Aula 05/CalcularMedia/src/java/br/com/ifc/util/Media.java
+++ Aulas/Aula 05/CalcularMedia/src/java/br/com/ifc/util/Media.java
@@ -4,21 +4,10 @@ import java.text.DecimalFormat;
 
 public class Media {
 
-    private final double notas[];
+    private double[] notas;
 
-    public Media(Double... notas) {
-        this.notas = new double[notas.length];
-        for (int i = 0; i < notas.length; i++) {
-            this.notas[i] = notas[i];
-        }
-    }
-
-    public Media(double n1, double n2, double n3, double n4) {
-        this.notas = new double[]{n1, n2, n3, n4};
-    }
-
-    public Media(double[] notas) {
-        this.notas = notas;
+    public Media(double[] notasDoServlet) {
+        this.notas = notasDoServlet;
     }
 
     public String calcularMedia() {
@@ -30,9 +19,4 @@ public class Media {
         double media = soma / notas.length;
         return df.format(media);
     }
-
-//    public static void main(String args[]){
-//        Media m = new Media(teste);
-//        System.out.println(m.calcularMedia());
-//    }
 }
